ca5bd53bd699a8fe2112efbb83bb0b4e6e10d1ed
Matteo Fabbri <matteo@phascode.org>
2018-05-15 11:24:37 +0200

    web service

---
+++ WindowsMonitor.Service/Controllers/SystemController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WindowsMonitor.Service.Controllers
+{
+    [Produces("application/json")]
+    //[Route("system")]
+    public class SystemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Json(WindowsMonitor.Win32.OperatingSystem.Retrieve());
+        }
+
+        public IActionResult Fans()
+        {
+            return Json(WindowsMonitor.Win32.Hardware.Cooling.Fan.Retrieve());
+        }
+    }
+}
--- WindowsMonitor.Service/Program.cs
+++ WindowsMonitor.Service/Program.cs
@@ -1,36 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Security.Permissions;
-using WindowsMonitor.CIM.Hardware;
-using WindowsMonitor.CIM.Hardware.Sensors;
-using WindowsMonitor.Win32.Hardware.Cooling;
-using WindowsMonitor.Win32.Hardware.Probes;
-using WindowsMonitor.Win32.Performance.Raw;
+using System.Management;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace WindowsMonitor.Service
 {
-    class Program
+    public class Program
     {
-        [SecurityPermission(SecurityAction.LinkDemand, UnmanagedCode = true)]
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var os = Win32.OperatingSystem.Retrieve().ToArray();
-            var currents = CurrentProbe.Retrieve().ToArray();
-            var voltages = VoltageProbe.Retrieve().ToArray();
-            var temperatures = TemperatureProbe.Retrieve().ToArray();
-
-            var processors = Processor.Retrieve().ToArray();
-            var processors2 = Win32.Hardware.Processor.Retrieve().ToArray();
-
-            try
-            {
-            }
-            catch (Exception e)
-            {
-                var n = e.GetType().Name;
-                Console.WriteLine(n);
-                throw;
-            }
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
