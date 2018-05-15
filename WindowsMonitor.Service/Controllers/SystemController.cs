cf7c8c61d13202c2ef69625c76c7b9663092af38
Matteo Fabbri <matteo@phascode.org>
2018-05-15 19:02:03 +0200

    web interface first stub

--- WindowsMonitor.Service/Controllers/SystemController.cs
+++ WindowsMonitor.Service/Controllers/SystemController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WindowsMonitor.Service.Controllers
 {
@@ -13,12 +8,12 @@ namespace WindowsMonitor.Service.Controllers
     {
         public IActionResult Index()
         {
-            return Json(WindowsMonitor.Win32.OperatingSystem.Retrieve());
+            return Json(Win32.OperatingSystem.Retrieve());
         }
 
         public IActionResult Fans()
         {
-            return Json(WindowsMonitor.Win32.Hardware.Cooling.Fan.Retrieve());
+            return Json(Win32.Hardware.DiskDrives.DiskDrivePhysicalMedia.Retrieve());
         }
     }
 }
--- WindowsMonitor.Service/wwwroot/js/site.js
+++ WindowsMonitor.Service/wwwroot/js/site.js
@@ -1,1 +1,14 @@
 ﻿// Write your JavaScript code.
+function toTable(data, container)
+{
+    data.forEach(obj =>
+    {
+        var table = $("<table>");
+        for (var key in obj)
+        {
+            table.append("<tr><td>" + key + "</td><td>" + obj[key] + "</td></tr>");
+        }
+
+        $(container).append(table);
+    });
+}
