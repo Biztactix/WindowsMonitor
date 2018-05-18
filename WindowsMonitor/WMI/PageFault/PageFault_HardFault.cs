710da69be874a19bb26ea90055362b38b3a3c7ad
Matteo Fabbri <matteo@phascode.org>
2018-05-18 12:20:04 +0200

    namespacing

--- WindowsMonitor/WMI/PageFault/PageFault_HardFault.cs
+++ WindowsMonitor/WMI/PageFault/PageFault_HardFault.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Management;
 
@@ -9,13 +7,13 @@ namespace WindowsMonitor.WMI
     /// </summary>
     public sealed class PageFault_HardFault
     {
-		public uint ByteCount { get; private set; }
-		public uint FileObject { get; private set; }
-		public uint Flags { get; private set; }
-		public dynamic InitialTime { get; private set; }
-		public ulong ReadOffset { get; private set; }
-		public uint TThreadId { get; private set; }
-		public uint VirtualAddress { get; private set; }
+        public uint ByteCount { get; private set; }
+        public uint FileObject { get; private set; }
+        public uint Flags { get; private set; }
+        public dynamic InitialTime { get; private set; }
+        public ulong ReadOffset { get; private set; }
+        public uint TThreadId { get; private set; }
+        public uint VirtualAddress { get; private set; }
 
         public static IEnumerable<PageFault_HardFault> Retrieve(string remote, string username, string password)
         {
@@ -47,13 +45,13 @@ namespace WindowsMonitor.WMI
             foreach (ManagementObject managementObject in objectCollection)
                 yield return new PageFault_HardFault
                 {
-                     ByteCount = (uint) (managementObject.Properties["ByteCount"]?.Value ?? default(uint)),
-		 FileObject = (uint) (managementObject.Properties["FileObject"]?.Value ?? default(uint)),
-		 Flags = (uint) (managementObject.Properties["Flags"]?.Value ?? default(uint)),
-		 InitialTime = (dynamic) (managementObject.Properties["InitialTime"]?.Value ?? default(dynamic)),
-		 ReadOffset = (ulong) (managementObject.Properties["ReadOffset"]?.Value ?? default(ulong)),
-		 TThreadId = (uint) (managementObject.Properties["TThreadId"]?.Value ?? default(uint)),
-		 VirtualAddress = (uint) (managementObject.Properties["VirtualAddress"]?.Value ?? default(uint))
+                    ByteCount = (uint) (managementObject.Properties["ByteCount"]?.Value ?? default(uint)),
+                    FileObject = (uint) (managementObject.Properties["FileObject"]?.Value ?? default(uint)),
+                    Flags = (uint) (managementObject.Properties["Flags"]?.Value ?? default(uint)),
+                    InitialTime = managementObject.Properties["InitialTime"]?.Value ?? default(dynamic),
+                    ReadOffset = (ulong) (managementObject.Properties["ReadOffset"]?.Value ?? default(ulong)),
+                    TThreadId = (uint) (managementObject.Properties["TThreadId"]?.Value ?? default(uint)),
+                    VirtualAddress = (uint) (managementObject.Properties["VirtualAddress"]?.Value ?? default(uint))
                 };
         }
     }
