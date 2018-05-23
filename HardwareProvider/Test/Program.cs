94ce4e2a7feebd329dfb1fdacd6294e44df654d0
Matteo Fabbri <matteo@phascode.org>
2018-05-24 00:41:08 +0200

    creating mini providers

---
+++ HardwareProvider/Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NvAPIWrapper;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NVIDIA.Initialize();
+
+            var cc = NvAPIWrapper.GPU.PhysicalGPU.GetPhysicalGPUs();
+
+            var m = new Mainboard.Hardware.Mainboard.Mainboard(null);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
